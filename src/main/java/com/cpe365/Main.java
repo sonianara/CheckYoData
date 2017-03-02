6c55d84c614baad8f0d8b3d60c91d1ba46986fa4
sonianara <sonianara96@gmail.com>
2017-03-02 11:50:42 -0800

    configured build path for javafx

---
+++ src/main/java/com/cpe365/Main.java
@@ -0,0 +1,18 @@
+package com.cpe365;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    // TODO Auto-generated constructor stub
+    
+  public void start(final Stage primaryStage) {
+
+    primaryStage.show();
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
