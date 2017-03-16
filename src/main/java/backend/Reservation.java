b29b9573e10b6cd05886b9fcde728b2d88bb3dd0
Courtney Brown <cbrown83@calpoly.edu>
2017-03-15 22:31:10 -0700

    might break travis

---
+++ src/main/java/backend/Reservation.java
@@ -0,0 +1,49 @@
+package backend;
+
+public class Reservation implements DatabaseObject {
+	private GymClass gymClass; 
+	private Member member;
+	
+	public Reservation(GymClass gymClass, Member member) {
+		this.gymClass = gymClass;
+		this.member = member;
+	}
+
+	public GymClass getGymClass() {
+		return gymClass;
+	}
+	public void setGymClass(GymClass gymClass) {
+		this.gymClass = gymClass;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	@Override
+	public String getKeys() {
+		return "member_email, class_id";
+	}
+
+	@Override
+	public String getValues() {
+		return "'" + member.getEmail() + "', '" + gymClass.getClassID();
+	}
+
+	@Override
+	public String getTable() {
+		return "reservations";
+	}
+
+	@Override
+	public String getKeyIdentifier() {
+		return "id";
+	} 
+	
+	public void addToDatabase() {
+		DatabaseCommunicator.addToDatabase(this);
+	}
+	
+}
--- src/main/java/frontend/MemberEntryController.java
+++ src/main/java/frontend/MemberEntryController.java
@@ -2,16 +2,20 @@ package frontend;
 
 import java.io.IOException;
 
+import javax.swing.GroupLayout.Alignment;
+
 import backend.DatabaseCommunicator;
 import backend.Member;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.Pane; 
-import javafx.scene.control.Button; 
-import javafx.scene.control.Label; 
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 /**
@@ -29,6 +33,7 @@ public class MemberEntryController {
 	@FXML private Label phoneNumber; 
 	@FXML private Label memberType; 
 	@FXML private Button editButton; 
+	@FXML private Button reservationButton; 
 	
 	/**
 	 * Method to inform the EditMemberController which member it is editing
@@ -36,7 +41,7 @@ public class MemberEntryController {
 	 */
 	@FXML
     private void editAction(ActionEvent event) throws IOException {	
-		Stage stage = new Stage();
+Stage stage = new Stage();
     	Pane myPane = null; 
 
     	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditMemberForm.fxml"));     
@@ -50,6 +55,28 @@ public class MemberEntryController {
 
 		
 		editMemberController.populateFields();
+	
+		Scene scene = new Scene(myPane); 
+    	stage.setScene(scene);    
+    	stage.show();   
+	}
+	
+	@FXML 
+	public void reserveAction(ActionEvent event) throws IOException {
+		Stage stage = new Stage();
+    	Pane myPane = null; 
+
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ClassRegistrationByMemberView.fxml"));     
+		myPane = (Pane)fxmlLoader.load();
+
+		// Sets values so the ResourceEntryController knows which course it contains.
+		ClassRegistrationByMemberController registrationController = fxmlLoader.<ClassRegistrationByMemberController>getController();
+		TextField memberName = (TextField) myPane.lookup("#memberName");
+		memberName.setText(member.getFirstName() + " " + member.getLastName());
+		memberName.setAlignment(Pos.CENTER);
+		memberName.setDisable(true); 
+		DatabaseCommunicator.getInstance(); 
+		registrationController.setMember(DatabaseCommunicator.getMember(phoneNumber.getText())); 
 	
 		Scene scene = new Scene(myPane); 
     	stage.setScene(scene);    
